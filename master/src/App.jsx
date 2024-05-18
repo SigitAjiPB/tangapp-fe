5c3a6c103080e05664a02cb9494723744f98b93f
unknown <sigitnj89@gmail.com>
2024-05-18 11:45:38 +0700

    Login

--- master/src/App.jsx
+++ master/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <wrapper className="flex relative h-[1000px]">
+      <div className="flex relative h-[1200px]">
 {/* Side Bar */}
-        <div class="flex h-screen flex-col justify-between border-e bg-white sticky top-0 left-0">
-    <div class="px-4 py-6">
-      <span class="grid h-10 w-32 place-content-center rounded-lg bg-sky-400 text-xs text-gray-600">
+        <div className="flex h-screen flex-col justify-between border-e bg-white sticky top-0 left-0">
+    <div className="px-4 py-6">
+      <span className="grid h-10 w-32 place-content-center rounded-lg bg-sky-400 text-xs text-gray-600">
         <img 
         src="./src/assets/logo.png" 
         alt="tangapp-logo"
@@ -20,44 +16,44 @@ function App() {
          />
       </span>
 
-      <ul class="mt-6 space-y-1">
+      <ul className="mt-6 space-y-1">
         <li>
           <a
             href="#"
-            class="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+            className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
           >
             General
           </a>
         </li>
 
         <li>
-          <details class="group [&_summary::-webkit-details-marker]:hidden">
+          <details className="group [&_summary::-webkit-details-marker]:hidden">
             <summary
-              class="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+              className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
             >
-              <span class="text-sm font-medium"> Teams </span>
+              <span className="text-sm font-medium"> Teams </span>
 
-              <span class="shrink-0 transition duration-300 group-open:-rotate-180">
+              <span className="shrink-0 transition duration-300 group-open:-rotate-180">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
+                  className="h-5 w-5"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
               </span>
             </summary>
 
-            <ul class="mt-2 space-y-1 px-4">
+            <ul className="mt-2 space-y-1 px-4">
               <li>
                 <a
                   href="#"
-                  class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                 >
                   Banned Users
                 </a>
@@ -66,7 +62,7 @@ function App() {
               <li>
                 <a
                   href="#"
-                  class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                 >
                   Calendar
                 </a>
@@ -78,7 +74,7 @@ function App() {
         <li>
           <a
             href="#"
-            class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+            className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
           >
             Billing
           </a>
@@ -87,23 +83,23 @@ function App() {
         <li>
           <a
             href="#"
-            class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+            className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
           >
             Invoices
           </a>
         </li>
 
         <li>
-          <details class="group [&_summary::-webkit-details-marker]:hidden">
+          <details className="group [&_summary::-webkit-details-marker]:hidden">
             <summary
-              class="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+              className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
             >
-              <span class="text-sm font-medium"> Account </span>
+              <span className="text-sm font-medium"> Account </span>
 
-              <span class="shrink-0 transition duration-300 group-open:-rotate-180">
+              <span className="shrink-0 transition duration-300 group-open:-rotate-180">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
+                  className="h-5 w-5"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
@@ -116,11 +112,11 @@ function App() {
               </span>
             </summary>
 
-            <ul class="mt-2 space-y-1 px-4">
+            <ul className="mt-2 space-y-1 px-4">
               <li>
                 <a
                   href="#"
-                  class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                 >
                   Details
                 </a>
@@ -129,7 +125,7 @@ function App() {
               <li>
                 <a
                   href="#"
-                  class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                 >
                   Security
                 </a>
@@ -139,7 +135,7 @@ function App() {
                 <form action="#">
                   <button
                     type="submit"
-                    class="w-full rounded-lg px-4 py-2 text-sm font-medium text-gray-500 [text-align:_inherit] hover:bg-gray-100 hover:text-gray-700"
+                    className="w-full rounded-lg px-4 py-2 text-sm font-medium text-gray-500 [text-align:_inherit] hover:bg-gray-100 hover:text-gray-700"
                   >
                     Logout
                   </button>
@@ -151,17 +147,17 @@ function App() {
       </ul>
     </div>
 
-    <div class="sticky inset-x-0 bottom-0 border-t border-gray-100 w-max">
-      <a href="#" class="flex items-center gap-2 bg-white p-4 hover:bg-gray-50">
+    <div className="sticky inset-x-0 bottom-0 border-t border-gray-100 w-max">
+      <a href="#" className="flex items-center gap-2 bg-white p-4 hover:bg-gray-50">
         <img
           alt=""
           src="../src/assets/yy.jpg"
-          class="size-10 rounded-full object-cover"
+          className="size-10 rounded-full object-cover"
         />
 
         <div>
-          <p class="text-xs">
-            <strong class="block font-medium">Sigit</strong>
+          <p className="text-xs">
+            <strong className="block font-medium">Sigit</strong>
 
             <span> Sigit@blockfuirt.com </span>
           </p>
@@ -172,10 +168,10 @@ function App() {
 {/* End Side Bar */}
 
 {/* Hero */}
-        <hero className='w-screen'>
+        <div className='w-screen relative'>
           {/* Announcement */}
-          <div class="flex items-center justify-between gap-4 bg-sky-400 px-4 py-3 text-white">
-            <p class="text-sm font-medium">
+          <div className="flex items-center justify-between gap-4 bg-sky-400 px-4 py-3 text-white">
+            <p className="text-sm font-medium">
               Mulailah mencatat hutang di 
               <a href="#" class="inline-block underline">Tangapp</a>
             </p>
@@ -186,9 +182,9 @@ function App() {
             >
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </button>
@@ -196,81 +192,82 @@ function App() {
           {/* End Announcement */}
 
           {/* Header */}
-          <header class="bg-white">
-  <div class="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+          <header className="bg-white drop-shadow-md sticky top-0">
+            <div class="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
 
-    <div class="flex flex-1 items-center justify-end md:justify-between">
-      <nav aria-label="Global" class="hidden md:block">
-        <ul class="flex items-center gap-6 text-lg">
-          <li>
-            <a class="text-sky-400 transition hover:text-sky-400/75" href="#"> About </a>
-          </li>
+              <div class="flex flex-1 items-center justify-end md:justify-between">
+                <nav aria-label="Global" class="hidden md:block">
+                  <ul class="flex items-center gap-6 text-lg">
+                    <li>
+                      <a class="text-sky-400 transition hover:text-sky-400/75" href="#"> About </a>
+                    </li>
 
-          <li>
-            <a class="text-sky-400 transition hover:text-sky-400/75" href="#"> Careers </a>
-          </li>
+                    <li>
+                      <a class="text-sky-400 transition hover:text-sky-400/75" href="#"> Careers </a>
+                    </li>
 
-          <li>
-            <a class="text-sky-400 transition hover:text-sky-400/75" href="#"> History </a>
-          </li>
+                    <li>
+                      <a class="text-sky-400 transition hover:text-sky-400/75" href="#"> History </a>
+                    </li>
 
-          <li>
-            <a class="text-sky-400 transition hover:text-sky-400/75" href="#"> Services </a>
-          </li>
+                    <li>
+                      <a class="text-sky-400 transition hover:text-sky-400/75" href="#"> Services </a>
+                    </li>
 
-          <li>
-            <a class="text-sky-400 transition hover:text-sky-400/75" href="#"> Projects </a>
-          </li>
+                    <li>
+                      <a class="text-sky-400 transition hover:text-sky-400/75" href="#"> Projects </a>
+                    </li>
 
-          <li>
-            <a class="text-sky-400 transition hover:text-sky-400/75" href="#"> Blog </a>
-          </li>
-        </ul>
-      </nav>
+                    <li>
+                      <a class="text-sky-400 transition hover:text-sky-400/75" href="#"> Blog </a>
+                    </li>
+                  </ul>
+                </nav>
 
-      <div class="flex items-center gap-4">
-        <div class="sm:flex sm:gap-4">
-          <a
-            class="block rounded-md bg-sky-400 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-sky-700"
-            href="#"
-          >
-            Login
-          </a>
+                <div class="flex items-center gap-4">
+                  <div class="sm:flex sm:gap-4">
+                  <a
+                    class="block rounded-md bg-sky-400 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-sky-500"
+                    href="#"
+                  >
+                    Login
+                  </a>
 
-          <a
-            class="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-sky-400 transition hover:text-sky-600/75 sm:block"
-            href="#"
-          >
-            Register
-          </a>
-        </div>
+                    <a
+                      class="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-sky-400 transition hover:text-sky-600/75 sm:block"
+                      href="#"
+                    >
+                      Register
+                    </a>
+                  </div>
 
-        <button
-          class="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
-        >
-          <span class="sr-only">Toggle menu</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
-      </div>
-    </div>
-  </div>
-</header>
+                  <button
+                    class="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
+                  >
+                    <span class="sr-only">Toggle menu</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path strokeLinecap="round" strokeLineJoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </header>   
           {/* End Header */}
-        </hero>
-{/* End Hero */}
+        </div>
+{/* End Hero */}          
 
 
-      </wrapper>
-    </>
+      </div>
+      </>
+
   )
 }
 
