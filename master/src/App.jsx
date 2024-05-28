07f345ebc9ca2abb3ca3a8b809a3e683a6836223
unknown <sigitnj89@gmail.com>
2024-05-28 15:21:24 +0700

    Add ScrollToTop Component

--- master/src/App.jsx
+++ master/src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import { BrowserRouter as Router, Routes,Route,} from 'react-router-dom';
 import Login from './pages/login'
 import LandingPage from './pages/landingPage';
+import ScrollToTop from './components/scrollToTop';
 
 function App() {
 
   return (
     <Router>
-      <Routes>
-        <Route path='*' Component={LandingPage}/>
-        <Route path='/login' Component={Login}/>
-      </Routes>
+        <ScrollToTop/>
+        <Routes>
+          <Route path='*' Component={LandingPage}/>
+          <Route path='/login' Component={Login}/>
+        </Routes>
     </Router>
   )
 }
